bd8907d5e7937a5cc85aa09fa438d64c912df5ab
vpdoes11 <vaistack.cs@gmail.com>
2026-02-20 14:10:38 +0530

    security: move Firebase API keys to .env variables

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBLr3C6u8ebOjBOkZZDPCXveEqwaUHw8WU",
-    authDomain: "cds-tracker-app.firebaseapp.com",
-    projectId: "cds-tracker-app",
-    storageBucket: "cds-tracker-app.firebasestorage.app",
-    messagingSenderId: "827101037286",
-    appId: "1:827101037286:web:8b73296722ddc3d0a67127",
-    measurementId: "G-K1GKCZD425"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
